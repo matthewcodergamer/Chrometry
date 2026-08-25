22a694256acfa17cd4bad33ac212858c442c8cba
matthewcodergamer <gamerstriper@gmail.com>
2026-08-25 00:49:01 -0400

    Cache production icon assets for Chrometry 1.3

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
-const CACHE='chrometry-v1-3-1';
-const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-liquid.svg'];
+const CACHE='chrometry-v1-3-2';
+const CORE=[
+  './','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-liquid.svg',
+  './apple-touch-icon.png','./icon-192.png','./icon-512.png'
+];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
