491705424f75d5732563c09f09eb0764a2224b15
matthewcodergamer <gamerstriper@gmail.com>
2026-08-25 01:38:02 -0400

    Bump final Chrometry cache and icon assets

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='chrometry-v1-3-6';
+const CACHE='chrometry-final-v1-4';
 const CORE=[
-  './','./index.html','./styles.css','./ios-polish.css','./app.js','./ui-polish.js','./manifest.webmanifest','./chrometry-icon.svg','./chrometry-favicon.svg',
-  './apple-touch-icon-v6.png','./icon-192-v6.png','./icon-512-v6.png'
+  './','./index.html','./styles.css','./ios-polish.css','./app.js','./ui-polish.js','./manifest.webmanifest','./chrometry-icon.svg','./chrometry-home-icon.svg','./chrometry-favicon.svg',
+  './apple-touch-icon-v7.png','./icon-192-v7.png','./icon-512-v7.png'
 ];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
