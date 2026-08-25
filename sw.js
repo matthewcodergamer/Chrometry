30b2cba7895301890d10dc7af1723f21a67696fa
matthewcodergamer <gamerstriper@gmail.com>
2026-08-25 01:49:11 -0400

    Refresh final Chrometry cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='chrometry-final-v1-4';
+const CACHE='chrometry-final-v1-4-1';
 const CORE=[
   './','./index.html','./styles.css','./ios-polish.css','./app.js','./ui-polish.js','./manifest.webmanifest','./chrometry-icon.svg','./chrometry-home-icon.svg','./chrometry-favicon.svg',
   './apple-touch-icon-v7.png','./icon-192-v7.png','./icon-512-v7.png'
