c8717c3aaa43f6aadd6a11ba7e3742d7df28c4d0
matthewcodergamer <gamerstriper@gmail.com>
2026-09-18 12:21:05 -0400

    Add public extension activation bridge configuration

--- chrometry-config.js
+++ chrometry-config.js
@@ -1,2 +1,3 @@
 window.CHROMETRY_API_BASE_URL = window.CHROMETRY_API_BASE_URL || '';
 window.CHROMETRY_WEB_URL = window.CHROMETRY_WEB_URL || 'https://matthewcodergamer.github.io/Chrometry/';
+window.CHROMETRY_EXTENSION_ID = window.CHROMETRY_EXTENSION_ID || '';
